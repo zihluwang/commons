45ab6856cdeb357e138cadba3506888b1a66dfc1
vorbote <theodore0126@outlook.com>
2021-06-04 11:44:45 +0800

    Updated some dependencies

--- src/test/java/cn/vorbote/commons/MapUtilTest.java
+++ src/test/java/cn/vorbote/commons/MapUtilTest.java
@@ -1,17 +1,23 @@
 package cn.vorbote.commons;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
 import java.io.PrintStream;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
+import java.util.logging.Logger;
 
+@Slf4j
 public class MapUtilTest {
 
     private final static PrintStream fmt = System.out;
 
     @Test
     public void run01() {
-
+        log.info("Hello World - {}", new Date());
     }
 }
